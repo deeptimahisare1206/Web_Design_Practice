14e546f7cfd54c2d4641293161e3427c6057db2c
deeptimahisare1206 <deeptimahisare@gmail.com>
2024-01-07 19:11:10 +0530

    new files added

---
+++ new/9oct.php
@@ -0,0 +1,16 @@
+<html>
+    <body>
+        <?php
+    // if ($post){
+    //     $number =$post["number"];
+    //     if($number%2==0){
+    //         print "$number is an even number." ;
+    //     }
+    //     else{
+    //         print "$number is an odd number.";
+    //     }
+    // }
+    echo $_POST["number"];
+    ?>
+    </body>
+</html>
